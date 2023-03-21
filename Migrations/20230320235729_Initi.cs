aaf43041418f0a3df613f9c8e5ef0f56527d7057
Levid <jassontejada@gmail.com>
2023-03-20 20:56:56 -0400

    Nueva BD y migraciones

---
+++ Migrations/20230320235729_Initi.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Parcial2_Levid.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Empacados",
+                columns: table => new
+                {
+                    EmpacadoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Concepto = table.Column<string>(type: "TEXT", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empacados", x => x.EmpacadoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    Costo = table.Column<float>(type: "REAL", nullable: false),
+                    Precio = table.Column<float>(type: "REAL", nullable: false),
+                    Existencia = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.ProductoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmpacadoDetalle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    EmpacadoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Cantidad = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmpacadoDetalle", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmpacadoDetalle_Empacados_EmpacadoId",
+                        column: x => x.EmpacadoId,
+                        principalTable: "Empacados",
+                        principalColumn: "EmpacadoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "Costo", "Descripcion", "Existencia", "Precio" },
+                values: new object[,]
+                {
+                    { 1, 2000f, "Mani", 200, 2600f },
+                    { 2, 3000f, "Pistachos", 400, 3600f },
+                    { 3, 1500f, "Pasas", 100, 2000f },
+                    { 4, 4000f, "Ciruela", 700, 5600f },
+                    { 5, 4000f, "Empaque en fundita", 700, 5600f }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmpacadoDetalle_EmpacadoId",
+                table: "EmpacadoDetalle",
+                column: "EmpacadoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmpacadoDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+
+            migrationBuilder.DropTable(
+                name: "Empacados");
+        }
+    }
+}
