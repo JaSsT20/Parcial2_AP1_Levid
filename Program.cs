e9b38f2bf9ee087006436a56d6c7344e1cb2da82
Levid <jassontejada@gmail.com>
2023-03-20 20:53:59 -0400

    Agregando Radzen al proyecto

--- Program.cs
+++ Program.cs
@@ -1,14 +1,25 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
+using Radzen;
 var builder = WebApplication.CreateBuilder(args);
 
 var ConStr = builder.Configuration.GetConnectionString("ConStr");
 builder.Services.AddDbContext<Contexto>(option => option.UseSqlite(ConStr));
 
+//BLLs
+builder.Services.AddScoped<EmpacadoBLL>();
+builder.Services.AddScoped<ProductosBLL>();
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+//Radzen
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
 
 var app = builder.Build();
 
